81dfc23333f038229d259606eb7602ed02a7f93c
Bindu gangalam <bindu_gangalam@epam.com>
2024-03-21 18:55:14 +0530

    Module 2 changes

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  title!: string
+description!: string
+creationDate!: Date
+duration!: number
+authors!: string[]
+
+@Input()
+editable:Boolean=true;
+
+@Input()
+ courses:[]=[];
+
+@Output() clickOnShow = new EventEmitter<void>();
+
+@Output() showCourse = new EventEmitter<void>();
+
+@Output() editCourse = new EventEmitter<void>();
+
+@Output() deleteCourse = new EventEmitter<void>();
+
+clickOnShowCourse()
+{
+  this.clickOnShow.emit();
+}
+
+showCourses()
+{
+  this.showCourse.emit();
+}
+
+editCourseData()
+{
+  this.editCourse.emit();
+}
+
+deleteCourseData()
+{
+  this.deleteCourse.emit();
+}
+
+}
