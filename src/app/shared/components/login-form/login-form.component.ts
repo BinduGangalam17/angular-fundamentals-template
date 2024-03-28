95a939b6e803aa7dd4713ca79892a907fe927805
Bindu gangalam <bindu_gangalam@epam.com>
2024-03-28 14:29:31 +0530

    Module3 Changes

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -9,4 +9,7 @@ import { NgForm } from '@angular/forms';
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
   //Use the names `email` and `password` for form controls.
+  onSubmit(arg0:any){
+    
+  }
 }
--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,10 +1,19 @@
 import { Directive } from "@angular/core";
-import { Validator } from "@angular/forms";
+import { AbstractControl, FormControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
 
 @Directive({
     selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: EmailValidatorDirective,
+        multi: true
+       }]
 })
 export class EmailValidatorDirective implements Validator {
+    validate(control: FormControl) {
+        const emailRegexp =  /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+        const valid = emailRegexp.test(control.value);
+        return valid ? null : {invalidEmail : true};
+    }
     // Add your code here
 }
