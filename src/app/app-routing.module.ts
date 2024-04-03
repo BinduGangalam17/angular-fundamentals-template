8dae8b12d406572397e23666039ee40ec0427624
Bindu gangalam <bindu_gangalam@epam.com>
2024-04-03 13:26:11 +0530

    Route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,8 @@ import { EditcourseComponent } from './shared/components/editcourse/editcourse.c
 import { CourseDetailsComponent } from './shared/components/course-details/course-details.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/courses', pathMatch: 'full' },
     { path: 'login', component: LoginFormComponent },
     { path: 'registration', component: RegistrationFormComponent },
     {path:'courses', component: CourseListComponent},
-    {path:'courses/add', component:AddcourseComponent},
-    {path:'courses/:id', component:CourseDetailsComponent},
-    {path:'courses/edit/:id', component:EditcourseComponent},
     {path:'**', component: CourseListComponent}
 ];
